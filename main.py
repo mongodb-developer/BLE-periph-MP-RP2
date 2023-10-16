00114244be6afda3b4bdbde657999ba5a1d3ed6e
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2023-10-16 13:50:36 +0200

    Add on-board temperature sensor reading

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from machine import Pin
+from machine import ADC, Pin
 import time
 
 led = Pin('LED', Pin.OUT)
+adc = ADC(4)
 
 while True:
   time.sleep_ms(500)
   led.toggle()
+  temperature =  27.0 - ((adc.read_u16() * 3.3 / 65535) - 0.706) / 0.001721
+  print("T: {}°C".format(temperature))
