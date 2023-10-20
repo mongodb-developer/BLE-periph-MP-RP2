ec0836666527e7bc122afab1f4cb6421bed47e36
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2023-10-20 12:02:35 +0200

    Add an Environmental Sensing Service with a temperature measurement characteristic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import aioble
 import bluetooth
 from machine import ADC, Pin
 from micropython import const
+import struct
 import uasyncio as asyncio
 
 _ADVERTISING_INTERVAL_US = const(200_000)
@@ -15,6 +16,9 @@ _CHAR_SERIAL_NUMBER_STR = bluetooth.UUID(0x2A25)
 _CHAR_FIRMWARE_REV_STR = bluetooth.UUID(0x2A26)
 _CHAR_HARDWARE_REV_STR = bluetooth.UUID(0x2A27)
 
+# Constants for the Environmental Sensing Service
+_SVC_ENVIRONM_SENSING = bluetooth.UUID(0x181A)
+_CHAR_TEMP_MEASUREMENT = bluetooth.UUID(0x2A1C)
 _TEMP_MEASUREMENT_INTERVAL_MS = const(15_000)
 
 svc_dev_info = aioble.Service(_SVC_DEVICE_INFO)
@@ -24,11 +28,18 @@ aioble.Characteristic(svc_dev_info, _CHAR_SERIAL_NUMBER_STR, read=True, initial=
 aioble.Characteristic(svc_dev_info, _CHAR_FIRMWARE_REV_STR, read=True, initial='0.0.1')
 aioble.Characteristic(svc_dev_info, _CHAR_HARDWARE_REV_STR, read=True, initial='0.0.1')
 
-aioble.register_services(svc_dev_info)
+svc_env_sensing = aioble.Service(_SVC_ENVIRONM_SENSING)
+temperature_char = aioble.Characteristic(svc_env_sensing, _CHAR_TEMP_MEASUREMENT, read=True)
+
+aioble.register_services(svc_dev_info, svc_env_sensing)
 
 connected = False
 led = Pin('LED', Pin.OUT)
 adc = ADC(4)
+
+def _encode_ieee11073(value, precision=2):
+    """ Binary representation of float value as IEEE-11073:20601 32-bit FLOAT """
+    return int(value * (10 ** precision)).to_bytes(3, 'little', True) + struct.pack('<b', -precision)
 
 async def task_peripheral():
   """ Task to handle advertising and connections """
@@ -39,11 +50,11 @@ async def task_peripheral():
       _ADVERTISING_INTERVAL_US,
       appearance=_APPEARANCE_MULTI_SENSOR,
       name='RP2-SENSOR',
-      services=[_SVC_DEVICE_INFO]
+      services=[_SVC_DEVICE_INFO, _SVC_ENVIRONM_SENSING]
     ) as connection:
       connected = True
       print("Connected from ", connection.device)
-      await connection.disconnected()
+      await connection.disconnected(timeout_ms=None)
       print("Disconnect")
 
 async def task_flash_led():
@@ -62,6 +73,7 @@ async def task_sensor():
   while True:
     temperature =  27.0 - ((adc.read_u16() * 3.3 / 65535) - 0.706) / 0.001721
     print("T: {}°C".format(temperature))
+    temperature_char.write(struct.pack("<B4s", 0, _encode_ieee11073(temperature)))
     await asyncio.sleep_ms(_TEMP_MEASUREMENT_INTERVAL_MS)
 
 async def main():
